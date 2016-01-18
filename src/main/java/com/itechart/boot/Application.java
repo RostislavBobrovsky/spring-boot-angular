d0ded568ed853dc38bff98b9ea4ed531c854668d
RostislavBobrovsky <rostislav.bobrovsky@gmail.com>
2016-01-18 18:31:21 +0300

    Replace properties to yaml

--- src/main/java/com/itechart/boot/Application.java
+++ src/main/java/com/itechart/boot/Application.java
@@ -1,6 +1,5 @@
 package com.itechart.boot;
 
-import com.itechart.boot.config.JpaConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(new Class<?>[]{Application.class, JpaConfiguration.class}, args);
+//		SpringApplication.run(new Class<?>[]{Application.class, JpaConfiguration.class}, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/main/java/com/itechart/boot/repository/DataRepository.java
+++ src/main/java/com/itechart/boot/repository/DataRepository.java
@@ -2,10 +2,11 @@ package com.itechart.boot.repository;
 
 import com.itechart.boot.domain.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
+@Repository
 public interface DataRepository extends JpaRepository<Data, Integer> {
 
     List<Data> findAll();
